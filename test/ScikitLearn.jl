5b98e06a6812fc9f78be1fbe17e52b9566456a85
Anthony D. Blaom <anthony.blaom@gmail.com>
2019-02-19 08:54:13 +1300

    Add tests, including real regression data (which now fails)

--- test/ScikitLearn.jl
+++ test/ScikitLearn.jl
@@ -3,13 +3,14 @@ module TestScikitLearn
 using MLJBase
 using Test
 
-
 import MLJModels
 import ScikitLearn
 using MLJModels.ScikitLearn_
+using CategoricalArrays
 
 task = load_iris();
 X, y = X_and_y(task)
+train, test = partition(eachindex(y), 0.6) # levels of y are split across split
 
 plain_classifier = SVMClassifier()
 nu_classifier = SVMNuClassifier()
@@ -19,21 +20,31 @@ plain_regressor = SVMRegressor()
 nu_regressor = SVMNuRegressor()
 linear_regressor = SVMLRegressor(max_iter=10000)
 
-Xr = [[0, 0], [1, 1]]
-yr = [0, 1]
-targetr = [[1, 1]]
+# Xr = [[0, 0], [1, 1]]
+# yr = [0, 1]
+# targetr = [[1, 1]]
 
-fitresultC, cacheC, reportC = MLJBase.fit(plain_classifier, 1, X, y);
-fitresultCnu, cacheCnu, reportCnu = MLJBase.fit(nu_classifier, 1, X, y);
-fitresultCL, cacheCL, reportCL = MLJBase.fit(linear_classifier, 1, X, y);
+task = load_boston()
+Xr, yr = X_and_y(task)
+
+fitresultC, cacheC, reportC = MLJBase.fit(plain_classifier, 1,
+                                          selectrows(X, train), y[train]);
+fitresultCnu, cacheCnu, reportCnu = MLJBase.fit(nu_classifier, 1, 
+                                          selectrows(X, train), y[train]);
+fitresultCL, cacheCL, reportCL = MLJBase.fit(linear_classifier, 1,
+                                          selectrows(X, train), y[train]);
 
 fitresultR, cacheR, reportR = MLJBase.fit(plain_regressor, 1, Xr, yr);
 fitresultRnu, cacheRnu, reportRnu = MLJBase.fit(nu_regressor, 1, Xr, yr);
 fitresultRL, cacheRL, reportRL = MLJBase.fit(linear_regressor, 1, Xr, yr);
 
-pcpred = predict(plain_classifier, fitresultC, X);
-nucpred = predict(nu_classifier, fitresultCnu, X);
-lcpred = predict(linear_classifier, fitresultCL, X);
+pcpred = predict(plain_classifier, fitresultC, selectrows(X, test));
+nucpred = predict(nu_classifier, fitresultCnu, selectrows(X, test));
+lcpred = predict(linear_classifier, fitresultCL, selectrows(X, test));
+
+@test levels(pcpred) == levels(y[train])
+@test levels(nucpred) == levels(y[train])
+@test levels(lcpred) == levels(y[train])
 
 rpred = predict(plain_regressor, fitresultR, targetr);
 nurpred = predict(nu_regressor, fitresultRnu, targetr);
