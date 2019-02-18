f64497317cc542dfbea3370fda1487746b7ad87f
Anthony D. Blaom <anthony.blaom@gmail.com>
2019-02-18 14:43:22 +1300

    update MLJBase

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,7 @@
 
 using Test
 using Pkg
-Pkg.add(PackageSpec(url="https://github.com/alan-turing-institute/MLJ.jl", rev="master")) 
+#Pkg.add(PackageSpec(url="https://github.com/alan-turing-institute/MLJ.jl", rev="master")) 
 
 @testset "DecisionTree" begin
   @test include("DecisionTree.jl")
