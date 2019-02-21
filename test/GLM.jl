172144b090925a9c195f67177803c58829497679
Thibaut Lienart <tlienart@me.com>
2019-02-21 14:56:38 +1100

    another set of fixes to decouple from MLJ, should work now

--- test/GLM.jl
+++ test/GLM.jl
@@ -1,12 +1,15 @@
 module TestGLM
 
-# using Revise
 using Test
-using MLJBase
 
+using MLJBase
 import MLJModels
-import GLM # MLJModels.GLM_ now available for loading
+import GLM
 using MLJModels.GLM_
+
+###
+### OLSREGRESSOR
+###
 
 task = load_boston()
 X, y = X_and_y(task)
@@ -15,25 +18,29 @@ train, test = partition(eachindex(y), 0.7)
 
 atom_ols = OLSRegressor()
 
-fitresult, cache, report = MLJBase.fit(atom_ols, 1, MLJBase.selectrows(X, train), y[train])
-p = predict(atom_ols, fitresult, MLJBase.selectrows(X, test))
+Xtrain = selectrows(X, train)
+ytrain = selectrows(y, train)
+Xtest  = selectrows(X, test)
 
-p = predict_mean(ols, MLJBase.selectrows(X, test))
+fitresult, _, _ = fit(atom_ols, 1, Xtrain, ytrain)
+
+p = predict_mean(atom_ols, fitresult, Xtest)
 
 # hand made regression to compare
 
 Xa = MLJBase.matrix(X) # convert(Matrix{Float64}, X)
 Xa1 = hcat(Xa, ones(size(Xa, 1)))
 coefs = Xa1[train, :] \ y[train]
-
 p2 = Xa1[test, :] * coefs
 
 @test p ≈ p2
 
 info(atom_ols)
 
-p_distr = predict(ols, MLJBase.selectrows(X, test))
+p_distr = predict(atom_ols, fitresult, selectrows(X, test))
 
+###
+### GLMCOUNT
 ###
 
 using Random: seed!
@@ -42,7 +49,7 @@ using Distributions
 seed!(0)
 
 X = randn(100, 3) .* randn(3)'
-Xtable = MLJ.table(X)
+Xtable = table(X)
 
 α = 0.1
 β = [-0.3, 0.2, -0.1]
@@ -52,12 +59,11 @@ y = [rand(Poisson(λᵢ)) for λᵢ ∈ λ]
 
 atom_glmcount = GLMCount()
 
-glmc = machine(atom_glmcount, Xtable, y)
-fit!(glmc)
+fitresult, _, _ = fit(atom_glmcount, 1, Xtable, y)
 
-p = predict_mean(glmc, Xtable)
+p = predict_mean(atom_glmcount, fitresult, Xtable)
 
-p_distr = predict(glmc, Xtable)
+p_distr = predict(atom_glmcount, fitresult, Xtable)
 
 end # module
 true
