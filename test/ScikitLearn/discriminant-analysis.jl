3c19a59170c9da162cf483e0e19e95d140882054
Thibaut Lienart <tlienart@me.com>
2019-11-03 22:18:08 +0000

    Adding LDA and  QDA from sklearn + registry  update (#119)
    * Adding LDA and  QDA from sklearn

---
+++ test/ScikitLearn/discriminant-analysis.jl
@@ -0,0 +1,24 @@
+Xc2, yc2 = gen_classif(classes=["A", "B", "C"])
+
+@testset "LDA" begin
+    m, f = simple_test_classif_prob(BayesianLDA(), Xc2, yc2)
+    fp = fitted_params(m, f)
+    @test keys(fp) == (:coef, :intercept, :covariance, :means,
+                       :priors, :scalings, :xbar, :classes,
+                       :explained_variance_ratio)
+    infos = info_dict(m)
+    @test infos[:input_scitype] == MLJBase.Table(MLJBase.Continuous)
+    @test infos[:target_scitype] == AbstractVector{<:MLJBase.Finite}
+    @test !isempty(infos[:docstring])
+end
+
+@testset "QDA" begin
+    m, f = simple_test_classif_prob(BayesianQDA(), Xc2, yc2)
+    fp = fitted_params(m, f)
+    @test keys(fp) == (:covariance, :means, :priors,
+                       :rotations, :scalings)
+    infos = info_dict(m)
+    @test infos[:input_scitype] == MLJBase.Table(MLJBase.Continuous)
+    @test infos[:target_scitype] == AbstractVector{<:MLJBase.Finite}
+    @test !isempty(infos[:docstring])
+end
