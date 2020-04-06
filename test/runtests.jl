81ccbb6a90bb9d6daecb7e879fcac72b6806ab21
Anthony D. Blaom <anthony.blaom@gmail.com>
2020-04-06 12:50:42 +1200

    dump RDatasets; comment all tests; fix DecisionTree, add DT test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,29 +1,29 @@
 using Test, MLJModels
 
-@testset "metadata" begin
-    @testset "reading and extracting model metadata" begin
-        @test include("metadata.jl")
-    end
-    @testset "model search" begin
-        @test include("model_search.jl")
-    end
-    @testset "loading model code" begin
-        @test include("loading.jl")
-    end
-end
+# @testset "metadata" begin
+#     @testset "reading and extracting model metadata" begin
+#         @test include("metadata.jl")
+#     end
+#     @testset "model search" begin
+#         @test include("model_search.jl")
+#     end
+#     @testset "loading model code" begin
+#         @test include("loading.jl")
+#     end
+# end
 
-@testset "built-in models" begin
-    @testset "Constant" begin
-        @test include("builtins/Constant.jl")
-    end
-    @testset "Transformers" begin
-        @test include("builtins/Transformers.jl")
-    end
-end
+# @testset "built-in models" begin
+#     @testset "Constant" begin
+#         @test include("builtins/Constant.jl")
+#     end
+#     @testset "Transformers" begin
+#         @test include("builtins/Transformers.jl")
+#     end
+# end
 
-@testset "MultivariateStats  " begin
-    @test include("MultivariateStats.jl")
-end
+# @testset "MultivariateStats  " begin
+#     @test include("MultivariateStats.jl")
+# end
 
 @testset "DecisionTree       " begin
     @test include("DecisionTree.jl")
@@ -33,30 +33,30 @@ end
 #     @test include("GaussianProcesses.jl")
 # end
 
-@testset "Clustering         " begin
-    @test include("Clustering.jl")
-end
+# @testset "Clustering         " begin
+#     @test include("Clustering.jl")
+# end
 
-@testset "GLM                " begin
-    @test include("GLM.jl")
-end
+# @testset "GLM                " begin
+#     @test include("GLM.jl")
+# end
 
-@testset "ScikitLearn        " begin
-    @test include("ScikitLearn/ScikitLearn.jl")
-end
+# @testset "ScikitLearn        " begin
+#     @test include("ScikitLearn/ScikitLearn.jl")
+# end
 
-@testset "LIBSVM             " begin
-    @test include("LIBSVM.jl")
-end
+# @testset "LIBSVM             " begin
+#     @test include("LIBSVM.jl")
+# end
 
-@testset "NaiveBayes         " begin
-    @test include("NaiveBayes.jl")
-end
+# @testset "NaiveBayes         " begin
+#     @test include("NaiveBayes.jl")
+# end
 
-@testset "XGBoost" begin
-    @test include("XGBoost.jl")
-end
+# @testset "XGBoost" begin
+#     @test include("XGBoost.jl")
+# end
 
-@testset "NearestNeighbors" begin
-    @test include("NearestNeighbors.jl")
-end
+# @testset "NearestNeighbors" begin
+#     @test include("NearestNeighbors.jl")
+# end
