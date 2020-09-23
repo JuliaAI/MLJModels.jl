a8fff419f0e108df168ce111ea8e051e558b40de
OkonSamuel <okonsamuel50@gmail.com>
2020-09-23 20:39:09 +0100

    Adds ignore keyword to FeatureSelector Transformer allow function as input to features keyword of FeatureSelector Transformer

--- src/builtins/Transformers.jl
+++ src/builtins/Transformers.jl
@@ -240,35 +240,99 @@ end
 ##
 
 """
-    FeatureSelector(features=Symbol[])
+    FeatureSelector(features=Symbol[], ignore=false)
 
 An unsupervised model for filtering features (columns) of a table.
 Only those features encountered during fitting will appear in
 transformed tables if `features` is empty (the default).
 Alternatively, if a non-empty `features` is specified, then only the
-specified features are used. Throws an error if a recorded or
-specified feature is not present in the transformation input.
+specified features encountered during fitting are used (`ignore=false`) or all features
+encountered during fitting which are not named in `features` are used (`ignore=true`).
+Throws an error if a recorded or specified feature is not present in the transformation 
+input.
+
+Instead of supplying a features vector, a Bool-valued callable can be
+also be specified. For example, specifying `FeatureSelector(features =
+name -> name in [:x1, :x3], ignore = true)` has the same effect as 
+`FeatureSelector(features = [:x1, :x3], ignore = true)`, namely to select
+ all features, with the exception of `:x1` and `:x3`.
+
+# Example
+
+```
+julia> using MLJModels, CategoricalArrays, MLJBase
+
+julia> X = (ordinal1 = [1, 2, 3],
+            ordinal2 = categorical([:x, :y, :x], ordered=true),
+            ordinal3 = [10.0, 20.0, 30.0],
+            ordinal4 = [-20.0, -30.0, -40.0],
+            nominal = categorical(["Your father", "he", "is"]));
+
+julia> select1 = FeatureSelector();
+
+julia> transform(fit!(machine(select1, X)), X)
+[ Info: Training Machine{FeatureSelector} @811.
+(ordinal1 = [1, 2, 3],
+ ordinal2 = CategoricalValue{Symbol,UInt32}[:x, :y, :x],
+ ordinal3 = [-1.0, 0.0, 1.0],
+ ordinal4 = [1.0, 0.0, -1.0],
+ nominal = CategoricalVale{String,UInt32}["Your father", "he", "is"],)
+
+julia> select2 = FeatureSelector(features=[:ordinal3, ], ignore=true);
+
+julia> transform(fit!(machine(select2, X)), X)
+[ Info: Training Machine{FeatureSelector} @721.
+(ordinal1 = [1, 2, 3],
+ ordinal2 = CategoricalValue{Symbol,UInt32}[:x, :y, :x],
+ ordinal4 = [-20.0, -30.0, -40.0],
+ nominal = CategoricalValue{String,UInt32}["Your father", "he", "is"],)
+
+```
+
 """
 @with_kw_noshow mutable struct FeatureSelector <: Unsupervised
-    features::Vector{Symbol} = Symbol[]
+    # features to be selected; empty means all
+    features::Union{Vector{Symbol}, Function}=Symbol[]
+    ignore::Bool = false # features to be ignored
 end
 
 function MLJBase.fit(transformer::FeatureSelector, verbosity::Int, X)
-    namesX = collect(Tables.schema(X).names)
-    if isempty(transformer.features)
-        fitresult = namesX
+    all_features = Tables.schema(X).names
+    
+    if transformer.features isa AbstractVector{Symbol}
+        if isempty(transformer.features)
+           features = collect(all_features) 
+        else
+            issubset(transformer.features, all_features) ||
+                throw(error("Attempting to select non-existent feature(s)."))
+            features = if transformer.ignore
+                filter!(all_features |> collect) do ftr
+                   !(ftr in transformer.features) 
+                end
+            else
+                transformer.features |> collect
+            end
+        end
     else
-        all(e -> e in namesX, transformer.features) ||
-            throw(error("Attempting to select non-existent feature(s)."))
-        fitresult = transformer.features
+        features = if transformer.ignore
+            filter!(all_features |> collect) do ftr
+                !(transformer.features(ftr))
+            end
+        else
+            filter!(all_features |> collect) do ftr
+                transformer.features(ftr)
+            end
+        end      
     end
+    
+    fitresult = features
     report = NamedTuple()
     return fitresult, nothing, report
 end
 
 MLJBase.fitted_params(::FeatureSelector, fitresult) = (features_to_keep=fitresult,)
 
-function MLJBase.transform(transformer::FeatureSelector, features, X)
+function MLJBase.transform(::FeatureSelector, features, X)
     all(e -> e in Tables.schema(X).names, features) ||
         throw(error("Supplied frame does not admit previously selected features."))
     return MLJBase.selectcols(X, features)
