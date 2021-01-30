2c80160beeba6e8ff391c10e114198cefffe7905
Anthony D. Blaom <anthony.blaom@gmail.com>
2021-01-30 18:00:33 +1300

    tweak doc-string

--- src/loading.jl
+++ src/loading.jl
@@ -81,12 +81,13 @@ end
 """
     @load ModelName pkg=nothing verbosity=0 add=false
 
-Import the model implementation code for the model named in the first
-argument into the calling module, specfying `pkg` in the case of
-duplicate names. Returns the model type.
+Import the model type the model named in the first argument into the
+calling module, specfying `pkg` in the case of an ambiguous name (to
+packages providing a model type with the same name). Returns the model
+type.
 
-**Warning** In older versions of MLJ/MLJModels, an *instance* was
-returned.
+**Warning** In older versions of MLJ/MLJModels, `@load` returned an
+*instance* instead.
 
 To automatically add required interface packages to the current
 environment, specify `add=true`. For interactive loading, use
