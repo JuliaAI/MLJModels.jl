2e6c43a2f0cac7eb2bffd1ee25bb924b5685c688
Dilum Aluthge <dilum@aluthge.com>
2021-03-29 19:41:35 -0400

    Update check_registry.jl

--- src/registry/src/check_registry.jl
+++ src/registry/src/check_registry.jl
@@ -18,9 +18,9 @@ function check_registry()
                 eval(program)
                 # add/refresh entry
                 print(rpad("Entry for $key was loaded properly ✓", 79)*"\r")
-            catch
+            catch ex
                 push!(problems, string(key))
-                @error "⚠ there was an issue trying to load $key"
+                @error "⚠ there was an issue trying to load $key" exception=ex
             end
         end
     end
