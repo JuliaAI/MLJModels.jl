d074b5d1033c4c6ec8780050e8e0faf9655fc850
Johanni Brea <jbrea@users.noreply.github.com>
2022-09-01 23:43:32 +0200

    fix?

--- src/init.jl
+++ src/init.jl
@@ -1,5 +1,6 @@
+const REGISTRY_PATH = @path joinpath(@__DIR__, "registry", "Project.toml")
 function __init__()
-    project = open(@path(joinpath(@__DIR__, "registry", "Project.toml"))) do io
+    project = open(REGISTRY_PATH) do io
         readlines(io)
     end
     global REGISTRY_PROJECT = Ref{Vector{String}}(project)
