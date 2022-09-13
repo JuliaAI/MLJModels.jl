9b142187cdeeb8084f350f41c0d4fc0d9d2dc5bb
Olivier Labayle <olabayle@gmail.com>
2022-09-13 14:30:08 +0100

    add InteractionTransformer

--- test/builtins/Transformers.jl
+++ test/builtins/Transformers.jl
@@ -662,5 +662,81 @@ end
 
 end
 
+#### INTERACTION TRANSFORMER ####
+
+@testset "Interaction Transformer functions" begin
+    # No column provided, A has scitype Continuous, B has scitype Count
+    table = (A = [1., 2., 3.], B = [4, 5, 6], C = ["x₁", "x₂", "x₃"])
+    @test MLJModels.actualcolumns(nothing, table) == (:A, :B)
+    # Column provided
+    @test MLJModels.actualcolumns([:A, :B], table) == (:A, :B)
+    # Column provided, not in table
+    @test_throws ArgumentError("Column(s) D are not in the dataset.") MLJModels.actualcolumns([:A, :D], table)
+    # Non Infinite scitype column provided
+    @test_throws ArgumentError("Column C's scitype is not Infinite.") MLJModels.actualcolumns([:A, :C], table)
+end
+
+
+@testset "Interaction Transformer" begin
+    # Check constructor sanity checks: order > 1, length(colnames) > 1
+    @test_throws ArgumentError("The order of interactions should be at least 2.") InteractionTransformer(order = 1)
+    @test_throws ArgumentError("At least 2 columns are required to compute interactions.") InteractionTransformer(colnames = [:A])
+
+    X = (A = [1, 2, 3], B = [4, 5, 6], C = [7, 8, 9])
+    # Default order=2, colnames=nothing, ie all columns
+    Xt = MLJBase.transform(InteractionTransformer(), nothing, X)
+    @test Xt == (
+        A = [1, 2, 3],
+        B = [4, 5, 6],
+        C = [7, 8, 9],
+        A_B = [4, 10, 18],
+        A_C = [7, 16, 27],
+        B_C = [28, 40, 54]
+    )
+    # order=3, colnames=nothing, ie all columns
+    Xt = MLJBase.transform(InteractionTransformer(order=3), nothing, X)
+    @test Xt == (
+        A = [1, 2, 3],
+        B = [4, 5, 6],
+        C = [7, 8, 9],
+        A_B = [4, 10, 18],
+        A_C = [7, 16, 27],
+        B_C = [28, 40, 54],
+        A_B_C = [28, 80, 162]
+    )
+    # order=2, colnames=[:A, :B], ie all columns
+    Xt =MLJBase.transform(InteractionTransformer(order=2, colnames=[:A, :B]), nothing, X)
+    @test Xt == (
+        A = [1, 2, 3],
+        B = [4, 5, 6],
+        C = [7, 8, 9],
+        A_B = [4, 10, 18]
+    )
+    # order=3, colnames=[:A, :B, :C], some non continuous columns
+    X = merge(X, (D = ["x₁", "x₂", "x₃"],))
+    Xt = MLJBase.transform(InteractionTransformer(order=3, colnames=[:A, :B, :C]), nothing, X)
+    @test Xt == (
+        A = [1, 2, 3],
+        B = [4, 5, 6],
+        C = [7, 8, 9],
+        D = ["x₁", "x₂", "x₃"],
+        A_B = [4, 10, 18],
+        A_C = [7, 16, 27],
+        B_C = [28, 40, 54],
+        A_B_C = [28, 80, 162]
+    )
+    # order=2, colnames=nothing, only continuous columns are dealt with
+    Xt = MLJBase.transform(InteractionTransformer(order=2), nothing, X)
+    @test Xt == (
+        A = [1, 2, 3],
+        B = [4, 5, 6],
+        C = [7, 8, 9],
+        D = ["x₁", "x₂", "x₃"],
+        A_B = [4, 10, 18],
+        A_C = [7, 16, 27],
+        B_C = [28, 40, 54],
+    )
+end
+
 end
 true
