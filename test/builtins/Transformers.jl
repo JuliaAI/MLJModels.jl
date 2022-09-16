3da589ddb085faadfef7c6a7ba06f5031095ab64
Olivier Labayle <olabayle@gmail.com>
2022-09-16 08:59:42 +0200

    rename colname(s) to feature(s)

--- test/builtins/Transformers.jl
+++ test/builtins/Transformers.jl
@@ -657,25 +657,25 @@ end
 @testset "Interaction Transformer functions" begin
     # No column provided, A has scitype Continuous, B has scitype Count
     table = (A = [1., 2., 3.], B = [4, 5, 6], C = ["x₁", "x₂", "x₃"])
-    @test MLJModels.actualcolumns(nothing, table) == (:A, :B)
+    @test MLJModels.actualfeatures(nothing, table) == (:A, :B)
     # Column provided
-    @test MLJModels.actualcolumns([:A, :B], table) == (:A, :B)
+    @test MLJModels.actualfeatures([:A, :B], table) == (:A, :B)
     # Column provided, not in table
-    @test_throws ArgumentError("Column(s) D are not in the dataset.") MLJModels.actualcolumns([:A, :D], table)
+    @test_throws ArgumentError("Column(s) D are not in the dataset.") MLJModels.actualfeatures([:A, :D], table)
     # Non Infinite scitype column provided
-    @test_throws ArgumentError("Column C's scitype is not Infinite.") MLJModels.actualcolumns([:A, :C], table)
+    @test_throws ArgumentError("Column C's scitype is not Infinite.") MLJModels.actualfeatures([:A, :C], table)
 end
 
 
 @testset "Interaction Transformer" begin
-    # Check constructor sanity checks: order > 1, length(colnames) > 1
+    # Check constructor sanity checks: order > 1, length(features) > 1
     @test_logs (:warn, "Constraint `model.order > 1` failed; using default: order=2.") InteractionTransformer(order = 1)
-    @test_logs (:warn, "Constraint `if model.colnames !== nothing\n"*
-                       "    length(model.colnames) > 1\nelse\n    true\nend` failed; "*
-                       "using default: colnames=nothing.") InteractionTransformer(colnames = [:A])
+    @test_logs (:warn, "Constraint `if model.features !== nothing\n"*
+                       "    length(model.features) > 1\nelse\n    true\nend` failed; "*
+                       "using default: features=nothing.") InteractionTransformer(features = [:A])
 
     X = (A = [1, 2, 3], B = [4, 5, 6], C = [7, 8, 9])
-    # Default order=2, colnames=nothing, ie all columns
+    # Default order=2, features=nothing, ie all columns
     Xt = MLJBase.transform(InteractionTransformer(), nothing, X)
     @test Xt == (
         A = [1, 2, 3],
@@ -685,7 +685,7 @@ end
         A_C = [7, 16, 27],
         B_C = [28, 40, 54]
     )
-    # order=3, colnames=nothing, ie all columns
+    # order=3, features=nothing, ie all columns
     Xt = MLJBase.transform(InteractionTransformer(order=3), nothing, X)
     @test Xt == (
         A = [1, 2, 3],
@@ -696,17 +696,17 @@ end
         B_C = [28, 40, 54],
         A_B_C = [28, 80, 162]
     )
-    # order=2, colnames=[:A, :B], ie all columns
-    Xt =MLJBase.transform(InteractionTransformer(order=2, colnames=[:A, :B]), nothing, X)
+    # order=2, features=[:A, :B], ie all columns
+    Xt =MLJBase.transform(InteractionTransformer(order=2, features=[:A, :B]), nothing, X)
     @test Xt == (
         A = [1, 2, 3],
         B = [4, 5, 6],
         C = [7, 8, 9],
         A_B = [4, 10, 18]
     )
-    # order=3, colnames=[:A, :B, :C], some non continuous columns
+    # order=3, features=[:A, :B, :C], some non continuous columns
     X = merge(X, (D = ["x₁", "x₂", "x₃"],))
-    Xt = MLJBase.transform(InteractionTransformer(order=3, colnames=[:A, :B, :C]), nothing, X)
+    Xt = MLJBase.transform(InteractionTransformer(order=3, features=[:A, :B, :C]), nothing, X)
     @test Xt == (
         A = [1, 2, 3],
         B = [4, 5, 6],
@@ -717,7 +717,7 @@ end
         B_C = [28, 40, 54],
         A_B_C = [28, 80, 162]
     )
-    # order=2, colnames=nothing, only continuous columns are dealt with
+    # order=2, features=nothing, only continuous columns are dealt with
     Xt = MLJBase.transform(InteractionTransformer(order=2), nothing, X)
     @test Xt == (
         A = [1, 2, 3],
