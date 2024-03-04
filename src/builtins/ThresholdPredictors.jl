7afa1fc4b9af85a82b24465ba8a8933b93ba57b9
Anthony D. Blaom <anthony.blaom@gmail.com>
2024-03-04 16:00:21 +1300

    add a test to catch serialization bug
    fix test
    
    oops

--- src/builtins/ThresholdPredictors.jl
+++ src/builtins/ThresholdPredictors.jl
@@ -305,6 +305,20 @@ function _predict_threshold(yhat::UnivariateFiniteArray{S,V,R,P,N},
 end
 
 
+## SERIALIZATION
+
+# function MMI.save(model::ThresholdUnion, fitresult)
+#     atomic_fitresult, threshold = fitresult
+#     atom = model.model
+#     return MMI.save(atom, atomic_fitresult), threshold
+# end
+# function MMI.restore(model::ThresholdUnion, serializable_fitresult)
+#     atomic_serializable_fitresult, threshold = serializable_fitresult
+#     atom = model.model
+#     return MMI.restore(atom, atomic_serializable_fitresult), threshold
+# end
+
+
 ## TRAITS
 
 # Note: input traits are inherited from the wrapped model
