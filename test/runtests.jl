6b2ac854f6578feed5f76c609f4190e46e1eec14
Dilum Aluthge <dilum@aluthge.com>
2024-12-12 08:37:28 -0500

    The issue with stdlib versions being fixed to 0.0.0 has been fixed in new versions of Julia

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,12 @@
-# The next two lines added as a workaround to
-# https://github.com/JuliaLang/Pkg.jl/issues/3628 (Julia 1.6):
 import Pkg
-Pkg.add(name="Statistics", version=VERSION)
+
+if Base.VERSION >= v"1.10-"
+    # The issue with stdlib versions being fixed to 0.0.0 has been fixed in new versions of Julia
+else
+    # The next line added as a workaround to
+    # https://github.com/JuliaLang/Pkg.jl/issues/3628 (Julia 1.6):
+    Pkg.add(name="Statistics", version=VERSION, uuid="10745b16-79ce-11e8-11f9-7d13ad32a3b2")
+end
 
 using Test, MLJModels
 
