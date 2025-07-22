95baee71ce68761b9fb18f4f2f28d7d542234da8
Anthony D. Blaom <anthony.blaom@gmail.com>
2025-07-22 17:28:28 +1200

    cleanup registry paths; mv registry_project to /src/; dump julia <1.10
    oops

--- src/init.jl
+++
@@ -1,7 +0,0 @@
-const REGISTRY_PATH = @path joinpath(@__DIR__, "registry", "Project.toml")
-function __init__()
-    project = open(REGISTRY_PATH) do io
-        readlines(io)
-    end
-    global REGISTRY_PROJECT = Ref{Vector{String}}(project)
-end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,17 +1,9 @@
 import Pkg
 
-if Base.VERSION >= v"1.10-"
-    # The issue with stdlib versions being fixed to 0.0.0 has been fixed in new versions of Julia
-else
-    # The next line added as a workaround to
-    # https://github.com/JuliaLang/Pkg.jl/issues/3628 (Julia 1.6):
-    Pkg.add(name="Statistics", version=VERSION, uuid="10745b16-79ce-11e8-11f9-7d13ad32a3b2")
-end
-
 using Test, MLJModels
 
-@testset "registry" begin
-    @test include(joinpath("..", "src", "registry", "test", "runtests.jl"))
+@testset "registry_project" begin
+    @test include("registry_project.jl")
 end
 
 @testset "metadata" begin
